a6a676fb5c00aeddc20b1f7db8807a8cac146ca9
joeycorbett <corbettjoey9@gmail.com>
2024-05-22 15:12:58 -0400

    Add drawing to grid
    Using an event listener to select each of the boxes and add a mouse enter effect which turns the background color of the divs black

--- script.js
+++ script.js
@@ -4,8 +4,17 @@ for (let i = 0; i < 16; i++) {
     for (let i = 0; i < 16; i++) {
         let div = document.createElement("div");
         div.style.border = "1px solid black";
-        div.style.width = "15px";
-        div.style.height = "15px";
+        div.style.width = "25px";
+        div.style.height = "25px";
+        div.classList.add("box")
         container.appendChild(div);
     }
 }
+
+const boxes = document.querySelectorAll(".box");
+
+boxes.forEach((box) => {
+    box.addEventListener("mouseenter", () => {
+        box.style.backgroundColor = "black";
+    });
+});
