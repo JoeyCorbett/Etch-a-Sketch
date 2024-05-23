e95e9284db7b62b39a75cd58ad0ce565cce8ee9c
joeycorbett <corbettjoey9@gmail.com>
2024-05-23 16:38:14 -0400

    Add ability to change grid size with button
    Divided container size by size of graph to obtain box size. This makes any graph size fit in the 500x500px container div

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 const container = document.querySelector(".grid-container");
+const gridBtn = document.querySelector(".gridBtn");
 
-for (let i = 0; i < 16; i++) {
-    for (let i = 0; i < 16; i++) {
+let gridSize = 16;
+
+function createGrid(size) {
+    let boxSize = 500 / size;
+    for (let i = 0; i < size * size; i++) {
         let div = document.createElement("div");
-        div.style.border = "1px solid black";
-        div.style.width = "25px";
-        div.style.height = "25px";
+        div.style.outline = "1px solid black";
+        div.style.height = `${boxSize}px`;
+        div.style.width = `${boxSize}px`;
         div.classList.add("box")
         container.appendChild(div);
     }
+    boxes = document.querySelectorAll(".box");
+
+    boxes.forEach((box) => {
+        box.addEventListener("mouseenter", () => {
+            box.style.backgroundColor = "black";
+        });
+    });
 }
 
-const boxes = document.querySelectorAll(".box");
+let boxes = document.querySelectorAll(".box");
 
-boxes.forEach((box) => {
-    box.addEventListener("mouseenter", () => {
-        box.style.backgroundColor = "black";
-    });
+createGrid(gridSize);
+
+gridBtn.addEventListener("click", () => {
+    let gridSize = prompt("Enter Grid Size: ");
+    if (gridSize > 100) {
+        alert("Maximum Size 100");
+    } else if (isNaN(gridSize)) {
+        alert("Must be a positive number")
+    }
+    else {
+        while (container.hasChildNodes()) {
+            container.removeChild(container.firstChild);
+        }
+        createGrid(gridSize);
+    }
 });
+
+
+
+
